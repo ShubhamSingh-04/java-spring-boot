7eb7d36795894ccbd4819834499c511ae753ee9e
Shubham <shubhamsinghmys@gmail.com>
2026-01-30 22:47:17 +0530

    custom application properties

--- src/main/java/com/udemy/course/first_spring_app/rest/FunRestController.java
+++ src/main/java/com/udemy/course/first_spring_app/rest/FunRestController.java
@@ -1,12 +1,21 @@
 package com.udemy.course.first_spring_app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class FunRestController {
-    // expose "/" that returns "Hello World"
 
+    @Value("${coder.name}")
+    private String name;
+
+    @Value("${coder.email}")
+    private String email;
+
+
+    // expose "/" that returns "Hello World"
     @GetMapping("/")
     public String sayHello(){
         return "Hello World4121!!";
@@ -15,5 +24,11 @@ public class FunRestController {
     @GetMapping("/hi")
     public String sayHi(){
         return "hi";
+    }
+
+//    Fetching custom properties
+    @GetMapping("/coder-info")
+    public String getCoderInfo(){
+        return "Name:" + name + ", email: " + email;
     }
 }
