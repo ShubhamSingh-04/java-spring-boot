1073ec1e3a54932824a38e4af36252d6cd783e52
Shubham <shubhamsinghmys@gmail.com>
2026-02-25 19:00:38 +0530

    @Primary

--- src/main/java/com/shubham/SpringCore/rest/DemoController.java
+++ src/main/java/com/shubham/SpringCore/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach coach;
 
     @Autowired
- DemoController(@Qualifier("springCoach") Coach coach){
+ DemoController(Coach coach){
      this.coach = coach;
  }
 
--- src/main/java/com/shubham/util/SqlCoach.java
+++ src/main/java/com/shubham/util/SqlCoach.java
@@ -1,8 +1,10 @@
 package com.shubham.util;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class SqlCoach implements Coach{
     @Override
     public String getInstruction(){
