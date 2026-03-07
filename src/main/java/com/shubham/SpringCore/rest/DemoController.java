b1e532101b231b6aa6314f2e680b99f663fc34d5
Shubham <shubhamsinghmys@gmail.com>
2026-03-07 19:32:35 +0530

    scope of bean

--- src/main/java/com/shubham/SpringCore/rest/DemoController.java
+++ src/main/java/com/shubham/SpringCore/rest/DemoController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach coach;
+    private Coach anotherCoach;
 
     @Autowired
- DemoController(Coach coach){ 
+ DemoController(@Qualifier("springCoach") Coach coach, @Qualifier("springCoach") Coach anotherCoach){
         System.out.println("This class is now initialized: " + getClass().getSimpleName());
+        System.out.println("DemoController - coach == anotherCoach: " + (coach == anotherCoach)); // false as scope is prototype
      this.coach = coach;
  }
 
--- src/main/java/com/shubham/util/SpringCoach.java
+++ src/main/java/com/shubham/util/SpringCoach.java
@@ -1,10 +1,13 @@
 package com.shubham.util;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SpringCoach implements Coach {
 
     SpringCoach(){
