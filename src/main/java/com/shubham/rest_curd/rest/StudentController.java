2b0db79585c2bd6d8dc909cacb4e1297e0090d0d
Shubham <shubhamsinghmys@gmail.com>
2026-05-02 20:03:13 +0530

    path variables

--- src/main/java/com/shubham/rest_curd/rest/StudentController.java
+++ src/main/java/com/shubham/rest_curd/rest/StudentController.java
@@ -1,7 +1,9 @@
 package com.shubham.rest_curd.rest;
 
 import com.shubham.rest_curd.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<Student>();
+
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<Student>();
 
         theStudents.add(new Student("Singh", "Shubham"));
         theStudents.add(new Student("S", "Harshita"));
         theStudents.add(new Student("P", "Deepak"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 }
